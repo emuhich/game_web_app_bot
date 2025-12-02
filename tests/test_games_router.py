66d00a69b7b96b76ef90b89858883bffb83fc5fb
muhametvafinegor <egor190420000@gmail.com>
2025-12-02 23:00:42 +0300

    Фикс оплаты

--- tests/test_games_router.py
+++ tests/test_games_router.py
@@ -1,8 +1,17 @@
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
 
 client = TestClient(app)
+
+
+@pytest.fixture(autouse=True)
+def init_fastapi_cache():
+    FastAPICache.init(InMemoryBackend(), prefix="test")
+    yield
+    FastAPICache.reset()
 
 
 def test_categories_endpoint(monkeypatch):
@@ -10,4 +19,3 @@ def test_categories_endpoint(monkeypatch):
     resp = client.get("/games/honesty/categories")
     assert resp.status_code == 200
     assert isinstance(resp.json(), list)
-
