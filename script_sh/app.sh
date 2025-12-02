3c6ff45d540112f225e23eec0f91b2ced86c1522
muhametvafinegor <egor190420000@gmail.com>
2025-12-02 16:51:15 +0300

    Деплой на сервер

--- script_sh/app.sh
+++ script_sh/app.sh
@@ -9,7 +9,7 @@ python /api/create_admin.py || echo "[WARN] create_admin.py failed"
 
 # Запускаем gunicorn/uvicorn, доверяя X-Forwarded-* заголовкам от всех прокси
 exec gunicorn app.main:app \
-  --workers 4 \
+  --workers 1 \
   --bind 0.0.0.0:8000 \
   --worker-class uvicorn.workers.UvicornWorker \
   --forwarded-allow-ips="*"
